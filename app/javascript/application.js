da801db89e2f144baa6da7c5249ae6f703ac51bd
odanielchristopher] <o.dani.br@gmail.com>
2025-01-09 22:58:32 -0300

    feat (front): iniciando a criacao do componente select

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,16 @@
 import "@hotwired/turbo-rails";
 import "controllers";
 
+document.addEventListener('DOMContentLoaded', () => {
+  const selects = document.querySelectorAll('.select');
+
+  selects.forEach((select) => {
+    select.addEventListener('change', ({ target }) => {
+      if (target) {
+        target.classList.add('filled'); // Adiciona data-filled se uma opção for selecionada
+      } else {
+        target.classList.remove('filled'); // Remove data-filled se a seleção for limpa
+      }
+    })
+  });
+});
