a280360a874a228d2e9821dda21cece9166f6e0a
odanielchristopher] <o.dani.br@gmail.com>
2025-01-10 14:14:52 -0300

    feat (front): criando as tarefas

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :set_task, only: %i[show edit update destroy]
   before_action :set_list, only: %i[new create]  # Adicionando callback para garantir que @list esteja disponível
 
   # GET /tasks or /tasks.json
   def index
-    @tasks = Task.where(list_id: params[:list_id])
-    render json: @tasks  # Corrigido para @tasks, não @lists
+    @tasks = Task.where(list_id: params[:list_id]) # Filtra tarefas com base no list_id
+    render json: @tasks  # Retorna todas as tarefas como JSON
   end
 
   # GET /tasks/1 or /tasks/1.json
@@ -14,7 +14,8 @@ class TasksController < ApplicationController
 
   # GET /tasks/new
   def new
-    @task = Task.new
+    @task = @list.tasks.new
+    @categories = Category.all # Carrega todas as categorias
   end
 
   # GET /tasks/1/edit
@@ -23,11 +24,12 @@ class TasksController < ApplicationController
 
   # POST /tasks or /tasks.json
   def create
-    @task = @list.tasks.build(task_params)  # Corrigido para usar @list
+    @task = @list.tasks.build(task_params)
+    @categories = Category.all
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: "Task was successfully created." }
+        format.html { redirect_to home_index_path(list_id: @list.id), notice: "Task was successfully created." }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +42,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: "Task was successfully updated." }
+        format.html { redirect_to home_index_path(list_id: @list.id), notice: "Task was successfully updated." }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -60,17 +62,17 @@ class TasksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    # Usar callbacks para compartilhar configuração ou restrições entre ações.
     def set_task
       @task = Task.find(params[:id])  # Corrigido para usar params[:id]
     end
 
-    # Only allow a list of trusted parameters through.
+    # Somente permitir uma lista de parâmetros confiáveis.
     def task_params
-      params.require(:task).permit(:titulo, :descricao, :horario, :list_id, :categoria_id)  # Corrigido para usar permit
+      params.require(:task).permit(:title, :description, :hour, :list_id, :category_id)  # Corrigido para os parâmetros corretos
     end
 
     def set_list
-      @list = List.find(params[:list_id])  # Certifique-se de que @list é setado para o create e new
+      @list = List.find(params[:list_id])  # Recupera a lista com base no list_id
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_09_113528) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_10_151841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -30,7 +30,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_09_113528) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "hour"
+    t.string "hour"
     t.bigint "list_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
