c26bf204806b8fa4a0fad3745dda8fa53bade425
daniel christopher <daniel.2005.christopher@gmail.com>
2025-08-04 13:56:20 -0300

    chore: setup shadcn

--- apps/fe/src/App.tsx
+++ apps/fe/src/App.tsx
@@ -1,36 +1,11 @@
-import { useState } from 'react';
+import { Button } from '@views/components/ui/Button';
+import { Input } from '@views/components/ui/Input';
 
-import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="content bg-card w-full max-w-[500px] flex-col items-center justify-center border shadow">
+      <Input placeholder="Write your name" />
+      <Button type="button">Submit</Button>
+    </div>
   );
 }
-
-export default App;
