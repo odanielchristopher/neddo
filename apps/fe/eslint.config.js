556a07dc10f6982cd9e62eba71373778fb9b1e75
daniel christopher <daniel.2005.christopher@gmail.com>
2025-08-04 12:56:18 -0300

    feat: setup root settings

---
+++ apps/fe/eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import baseConfig from "../../eslint.config.mjs";
+
+export default defineConfig([
+  ...baseConfig,
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    }
+  }
+]);
