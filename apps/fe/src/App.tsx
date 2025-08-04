0870936719736346cde30b9a00793d8f4a0ccac5
daniel christopher <daniel.2005.christopher@gmail.com>
2025-08-04 16:26:03 -0300

    save

--- apps/fe/src/App.tsx
+++ apps/fe/src/App.tsx
@@ -4,6 +4,7 @@ import { Input } from '@views/components/ui/Input';
 export function App() {
   return (
     <div className="content bg-card w-full max-w-[500px] flex-col items-center justify-center border shadow">
+      
       <Input placeholder="Write your name" />
       <Button type="button">Submit</Button>
     </div>
