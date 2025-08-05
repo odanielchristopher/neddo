171b46d76f8c45d9eed9dca79007d4cd12828ec6
daniel christopher <daniel.2005.christopher@gmail.com>
2025-08-05 10:30:20 -0300

    chore: settup ui theme

--- apps/fe/src/App.tsx
+++ apps/fe/src/App.tsx
@@ -1,11 +1,21 @@
+import { ThemeProvider } from '@app/contexts/ThemeProvider';
 import { Button } from '@views/components/ui/Button';
 import { Input } from '@views/components/ui/Input';
 
 export function App() {
   return (
-    <div className="content bg-card w-full max-w-[500px] flex-col items-center justify-center border shadow">
-      <Input placeholder="Write your name" />
-      <Button type="button">Submit</Button>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="neddo:ui-theme">
+      <div className="grid h-full w-full place-items-center">
+        <div className="bg-card rounded-2xl border p-4 shadow">
+          <div className="flex flex-col gap-3">
+            <Input placeholder="Nome" />
+            <Input placeholder="Sobrenome" />
+          </div>
+          <Button type="button" className="mt-6 w-full">
+            Submit
+          </Button>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
