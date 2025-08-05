c739135866c0e387c7b0ee0fcf3d8154a5fdcfaf
daniel christopher <daniel.2005.christopher@gmail.com>
2025-08-05 09:43:57 -0300

    chore: test

--- apps/fe/src/App.tsx
+++ apps/fe/src/App.tsx
@@ -4,7 +4,6 @@ import { Input } from '@views/components/ui/Input';
 export function App() {
   return (
     <div className="content bg-card w-full max-w-[500px] flex-col items-center justify-center border shadow">
-      
       <Input placeholder="Write your name" />
       <Button type="button">Submit</Button>
     </div>
